8964386306a45a1bc52e0a1d87148d9dc5c0e80b
Aidan Chang <119556552+plinygg@users.noreply.github.com>
2025-04-06 14:30:36 -0500

    lesson #3

---
+++ cs in python/3.py
@@ -0,0 +1,15 @@
+print(5 > 3)
+print(100 < 2)
+
+print("hello!" == "hello")
+
+number = input("Input a number")
+number = int(number)
+
+if number > 100:
+    print("your number is larger than 100.")
+elif number > 50:
+    print("your number is greater than 50")
+else:
+    print("dang, your number is either 50 or its less than 50.")
+    
